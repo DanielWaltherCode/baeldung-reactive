852992dcaa0131a404ad9b5cdde16bda6edf037d
Daniel Walther <daniel.b.walther@gmail.com>
2021-08-11 21:08:48 +0200

    Added new simple Unit test for endpoint

--- src/main/java/com/example/reactive/Foo.java
+++ src/main/java/com/example/reactive/Foo.java
@@ -8,12 +8,20 @@ public class Foo {
         this.id = id;
         this.name = name;
     }
-    
+
     public Long getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/test/java/com/example/reactive/ReactiveApplicationTests.java
+++ src/test/java/com/example/reactive/ReactiveApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example.reactive;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import org.reactivestreams.Subscription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.BaseSubscriber;
 
-@SpringBootTest
+
+@WebFluxTest(controllers = ReactiveController.class)
 class ReactiveApplicationTests {
+
+    @Autowired
+    private WebTestClient webClient;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void testReactiveController() {
+        FluxExchangeResult<Foo> response = webClient.get().uri("/foo")
+                .header(HttpHeaders.ACCEPT, "text/event-stream")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentTypeCompatibleWith(MediaType.TEXT_EVENT_STREAM)
+                .returnResult(Foo.class);
+
+        response.getResponseBody().subscribe(new BaseSubscriber<>() {
+            @Override
+            public void hookOnNext(Foo value) {
+                assertNotNull(value);
+                assertEquals(0, (long) value.getId());
+                assertEquals(value.getName(), "Foo Flux");
+            }
+        });
+
+    }
 }
